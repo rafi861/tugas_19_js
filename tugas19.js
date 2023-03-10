fd4d81cae36e66a285083c293bf386e9457234f2
Muhammad Rafi <muhammadrafi2545@gmail.com>
2023-03-10 18:10:56 +0700

    tugas 19 js

---
+++ tugas19.js
@@ -0,0 +1,16 @@
+function tugas() {
+  var smgt = "ayo semangat...";
+  console.log(smgt.repeat(3));
+  console.log();
+  console.log("*".repeat(5));
+  console.log("*".repeat(20));
+  console.log("*".repeat(30));
+  console.log("*".repeat(40));
+  console.log("*".repeat(50));
+  console.log("*".repeat(40));
+  console.log("*".repeat(30));
+  console.log("*".repeat(20));
+  console.log("*".repeat(5));
+
+}
+tugas()
